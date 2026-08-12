f9271e82153a51a35184fadb37dbf3183ab13898
Danilo Santos <danilo.santos@cyberland.edu.sg>
2026-08-12 10:00:15 +0800

    Added: Project Service and Enforce Traits

--- koda-assessment-api/app/Http/Controllers/ProjectController.php
+++ koda-assessment-api/app/Http/Controllers/ProjectController.php
@@ -5,12 +5,21 @@ namespace App\Http\Controllers;
 use App\Models\Project;
 use App\Http\Resources\ProjectResource;
 use Illuminate\Http\Request;
+use App\Traits\AdvancedFilterTrait;
+use App\Traits\HttpResponse;
+use Illuminate\Database\Eloquent\Builder;
 use Taksu\Restful\Controllers\CrudController;
+use Taksu\Restful\Traits\ModelCommonTrait;
+use Exception;
+use App\Services\ProjectService;
+use Illuminate\Support\Arr;
 
 class ProjectController extends CrudController
 {
 
-    public function __construct()
+    use AdvancedFilterTrait, HttpResponse;
+
+    public function __construct(protected ProjectService $service)
     {
         parent::__construct(Project::class, ProjectResource::class);
     }
